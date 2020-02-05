aa5d8be8f88f2981c39c6c814d3b7bcd4865a27a
frazierprime <joshfrazier2113@gmail.com>
2020-02-05 11:55:44 -0800

    Cleans up project euler code for merge

--- project_euler/euler_five.py
+++ project_euler/euler_five.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env
+#!/usr/bin/env python3
 
 # 2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
 
--- project_euler/euler_nine.py
+++ project_euler/euler_nine.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env
+#!/usr/bin/env python3
 
 # A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
 # a^2 + b^2 = c^2
